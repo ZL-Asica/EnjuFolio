05f9a96fd97c5e99990718d0dbbe2b26e3a232cd
ZL Asica <40444637+ZL-Asica@users.noreply.github.com>
2025-08-20 20:05:40 -0500

    chore(deps): bump up frontend dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,16 @@
 import antfu from '@antfu/eslint-config'
-import nextPlugin from '@next/eslint-plugin-next'
 
 export default antfu({
   formatters: true,
-  react: true,
+  nextjs: true,
   typescript: {
     tsconfigPath: 'tsconfig.json',
-  },
-  plugins: {
-    '@next/next': nextPlugin,
   },
   lessOpinionated: true,
   rules: {
     // For Next.js JSON-LD we need to allow the use of `dangerouslySetInnerHTML`
     'react-dom/no-dangerously-set-innerhtml': 'off',
-  }
+  },
 }, {
   files: ['./src/components/Loadings/**/*.ts', './src/components/Loadings/**/*.tsx', './src/app/**/loading.tsx'],
   rules: {
